c92889fa306790135febf2d80a0b173060e35484
OussamaAlouat <al317257@uji.es>
2023-11-30 12:46:46 +0100

    FIX: extract on a constants file

---
+++ src/app/constants/constants.ts
@@ -0,0 +1,17 @@
+ export const NAV_OPTIONS = [
+  {
+    name: 'Home',
+    id: 0,
+    anchor: 'home'
+  },
+  {
+    name: 'About',
+    id: 1,
+    anchor: 'about'
+  },
+  {
+    name: 'Contact',
+    id: 2,
+    anchor: 'contact'
+  }
+];
